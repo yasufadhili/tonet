753c872ebb5ad0b69ef708e319cb9a8b300966a1
Yasu Fadhili <yasufadhili@gmail.com>
2024-11-11 21:47:42 +0300

    Setup intial network core setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 
 #include "core/utils/logger.h"
-//#include "core/utils/thread.h"
+#include "core/utils/thread.h"
 #include "cli/console.h"
 #include <iostream>
-
-
+#include "core/network/socket.h"
 
 
 int main(){
 
-  cli::Console console;
+  std::string name {"yasu"};
+
+  utils::Logger logger("logging_example.log");
+
+  logger.log("Welcome \n");
+  logger.log("Name % :", name);
+
+  std::cout << "main exiting." << std::endl;
   
 }
 
