555a67cb614b339ddff6c285e4903a9398250ccc
Yasu Fadhili <yasufadhili@gmail.com>
2024-11-11 20:44:04 +0300

    Updated thread

--- src/core/utils/thread.h
+++ src/core/utils/thread.h
@@ -8,6 +8,14 @@
 #include <sys/syscall.h>
 
 namespace utils{
+
+  inline auto setThreadCore(int core_id) noexcept {
+    cpu_set_t cpuset;
+    CPU_ZERO (&cpuset);
+    CPU_SET(core_id, &cpuset);
+    return (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) == 0);
+  }
+
   template<typename T, typename... A>
   inline auto createAndStartThread(int core_id, const std::string& name, T &&func, A &&... args) noexcept {
     std::atomic<bool> running(false), failed(false);
@@ -36,10 +44,5 @@ namespace utils{
     }
     return t;
   }
-  inline auto setThreadCore(int core_id) noexcept {
-    cpu_set_t cpuset;
-    CPU_ZERO (&cpuset);
-    CPU_SET(core_id, &cpuset);
-    return (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) == 0);
-  }
-};
+
+}; // namespace utils
