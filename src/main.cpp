d563b3ffd743db793c34874b76ff992171fe49fe
Yasu Fadhili <yasufadhili@gmail.com>
2024-11-11 15:44:38 +0300

    Added thread util. Though not yet complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
 
 #include "core/utils/logger.h"
+//#include "core/utils/thread.h"
 #include <iostream>
+
+
+
 
 int main(){
 
-  try {
-    utils::Logger::getInstance().init("tonet.log", utils::LogLevel::DEBUG);
-    utils::LOG_INFO("Tonet application starting...");
-        
-    // ....
-        
-  } catch (const std::exception& e) {
-    std::cerr << "Failed to initialise logger: " << e.what() << std::endl;
-    return 1;
-  }
+  
   
 }
+
